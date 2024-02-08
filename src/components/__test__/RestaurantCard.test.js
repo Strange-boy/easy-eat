5d4a6c0e89d751b418dec3757f250eff70cd0046
Joel <joel.panvel@gmail.com>
2024-02-08 18:02:14 +0530

    writing unit test for higher order components(Best selling feature)

--- src/components/__test__/RestaurantCard.test.js
+++ src/components/__test__/RestaurantCard.test.js
@@ -1,17 +1,34 @@
 
-import { render, screen } from "@testing-library/react";
-import RestaurantCards from "../RestaurantCards";
+import { render, screen, waitFor } from "@testing-library/react";
+import RestaurantCards, {withBestSellingRestro} from "../RestaurantCards";
+
 import MOCK_DATA from "../mocks/RestaurantCardData.json"
 import "@testing-library/jest-dom"
 
-it("should render the restaurant card data along with restaurant data", () => {
 
-    //first we must render the component
-    render(<RestaurantCards resData={MOCK_DATA}/>)
+describe("In order to test the restaurant card component", () => {
+    it("should render the restaurant card data along with restaurant data", () => {
 
-    //now we have must query heading 
-    const restaurantName = screen.getByText("Thai's Kitchen");
+        //first we must render the component
+        render(<RestaurantCards resData={MOCK_DATA}/>)
+    
+        //now we have must query heading 
+        const restaurantName = screen.getByText("Arabian Shake Cafe -Indira Jn");
+    
+        //now we have to state the assertion
+        expect(restaurantName).toBeInTheDocument(); 
+    })
 
-    //now we have to state the assertion
-    expect(restaurantName).toBeInTheDocument(); 
+
+    //in order to render the best selling restaurant
+    const BestSellerRestro = withBestSellingRestro(RestaurantCards);
+    it("should render the higher order component", async () => {
+        //render the component
+        render(<BestSellerRestro resData={MOCK_DATA} />)
+
+        
+        const bestSellingLabel = screen.getByText("Best Seller");
+        expect(bestSellingLabel).toBeInTheDocument();
+    })
 })
+
